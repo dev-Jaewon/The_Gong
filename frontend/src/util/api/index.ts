26f15a2d7a9fabf10ebc6a42b62fad64f8416b91
dev-Jaewon <app235naver@gmail.com>
2023-05-09 01:21:16 +0900

    [fix] vite 번들러 환경변수 못 불러오는 오류 해결

--- frontend/src/util/api/index.ts
+++ frontend/src/util/api/index.ts
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.VITE_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
+  timeout: 2000,
 });
 
-api.defaults.timeout = 2000;
+if (process.env.NODE_ENV === 'production') {
+  api({ baseURL: import.meta.env.VITE_BASE_URL });
+}
 
 export { api };
