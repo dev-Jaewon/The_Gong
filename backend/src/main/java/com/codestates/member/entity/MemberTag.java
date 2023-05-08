41abfcb50a7c455c9512f8339d2eec1327c89267
gtaegyeong <gangtaegyeong12@gmail.com>
2023-05-08 18:18:08 +0900

    setup : member 로직 구현

--- backend/src/main/java/com/codestates/member/entity/MemberTag.java
+++ backend/src/main/java/com/codestates/member/entity/MemberTag.java
@@ -2,6 +2,7 @@ package com.codestates.member.entity;
 
 import com.codestates.tag.entity.Tag;
 import com.codestates.common.entity.BaseEntity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
