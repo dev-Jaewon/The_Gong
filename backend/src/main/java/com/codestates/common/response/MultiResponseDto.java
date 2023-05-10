40920ba693c69edd3931176b2a343f4ce0cb477d
gtaegyeong <gangtaegyeong12@gmail.com>
2023-05-10 15:03:23 +0900

    feat : 응답 Dto 패키지수정

--- backend/src/main/java/com/codestates/common/response/MultiResponseDto.java
+++ backend/src/main/java/com/codestates/common/response/MultiResponseDto.java
@@ -1,4 +1,4 @@
-package com.codestates.common.dto;
+package com.codestates.common.response;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
