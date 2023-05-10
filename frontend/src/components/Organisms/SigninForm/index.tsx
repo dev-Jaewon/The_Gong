c8f810398197fbde3c6f009763e3dfd169055d61
dev-Jaewon <app235naver@gmail.com>
2023-05-10 20:58:12 +0900

    [feat] login organisms component 생성 및 api 미 개발로 인해 샘플 코드 작성

---
+++ frontend/src/components/Organisms/SigninForm/index.tsx
@@ -0,0 +1,88 @@
+import styled from 'styled-components';
+import useForm from '../../../hooks/useForm';
+import { api } from '../../../util/api';
+import { Button } from '../../atoms/Button';
+import { InputLabel } from '../../moecules/InputLabel';
+import { useMutation } from '@tanstack/react-query';
+import { SIGNIN_VALID_MESSAGE } from '../../../constans';
+import { useState } from 'react';
+
+type SigninData = {
+  email: string;
+  password: string;
+};
+
+export const SigninForm = () => {
+  const [fetchErrorMessage, setFetchErrorMessage] = useState<string>('');
+  const { data, errors, handleChange, handleSubmit } = useForm<SigninData>({
+    validations: {
+      email: {
+        pattern: {
+          value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+          message: SIGNIN_VALID_MESSAGE.email,
+        },
+      },
+      password: {
+        required: {
+          value: true,
+          message: 'password를 입력해주세요.',
+        },
+      },
+    },
+    onSubmit: handleSigninFetch,
+  });
+
+  const mutation = useMutation({
+    mutationFn: async (data: SigninData) => api.post('/signin', data),
+    onError: () => setFetchErrorMessage(SIGNIN_VALID_MESSAGE.login),
+  });
+
+  function handleSigninFetch() {
+    console.log(123);
+
+    mutation.mutate(data);
+  }
+
+  console.log(errors);
+
+  return (
+    <ContainerForm onSubmit={handleSubmit}>
+      <InputLabel
+        placeholder="email"
+        errorMessage={errors.email}
+        onChange={handleChange('email')}
+        isValid={fetchErrorMessage || errors.email ? false : true}
+      />
+      <InputLabel
+        type="password"
+        placeholder="password"
+        errorMessage={errors.password}
+        onChange={handleChange('password')}
+        isValid={fetchErrorMessage || errors.password ? false : true}
+      />
+      {fetchErrorMessage && <ErrorMessage>{fetchErrorMessage}</ErrorMessage>}
+      <Button fillColor>로그인</Button>
+    </ContainerForm>
+  );
+};
+
+const ContainerForm = styled.form`
+  gap: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  max-width: 400px;
+  font-family: Noto Sans KR;
+
+  button {
+    margin-top: 20px;
+  }
+`;
+
+const ErrorMessage = styled.p`
+  position: relative;
+  font-size: 13px;
+  color: red;
+  transform: translateY(-10px);
+`;
