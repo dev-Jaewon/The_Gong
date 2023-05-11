07b6e0ce23d5ff9cbda12422f40460fcb7e02e0a
SOONUK-k <kswnuguri@gmail.com>
2023-05-11 15:58:44 +0900

    Chore : 테스트를 위한 변경 원복

--- backend/src/main/java/com/codestates/auth/config/SecurityConfiguration.java
+++ backend/src/main/java/com/codestates/auth/config/SecurityConfiguration.java
@@ -36,71 +36,71 @@ import java.util.Arrays;
 @EnableWebSecurity(debug = true)
 public class SecurityConfiguration {
 
-//    private final JwtTokenizer jwtTokenizer;
-//    private final UserDetailsService userDetailsService;
-//
-//    @Bean
-//    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
-//
-//        http
-//                .headers().frameOptions().sameOrigin()
-//                .and()
-//                .csrf().disable()
-//                .cors(Customizer.withDefaults())
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//
-//                .and()
-//                .formLogin().disable()
-//                .httpBasic().disable()
-//                .apply(new CustomFilterConfigurer())
-//                .and()
-//
-//                .exceptionHandling()
-//                .authenticationEntryPoint(new MemberAuthenticationEntryPoint())
-//                .accessDeniedHandler(new MemberAccessDeniedHandler())
-//
-//                .and()
-//                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
-//
-//        return http.build();
-//    }
-//
-//
-//
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration cors = new CorsConfiguration();
-////        cors.setAllowCredentials(true);
-//        cors.setAllowedOrigins(Arrays.asList("*")); //"http://localhost:3000"
-//        cors.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE"));
-////        cors.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", cors);
-//        return source;
-//    }
-//
-//
-//
-//    public class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
-//        @Override
-//        public void configure(HttpSecurity builder) throws Exception {
-//
-//            AuthenticationManager manager = builder.getSharedObject(AuthenticationManager.class);
-//            JwtAuthenticationFilter authentication = new JwtAuthenticationFilter(manager, jwtTokenizer);
-//            authentication.setFilterProcessesUrl("/members/login");
-//            authentication.setAuthenticationSuccessHandler(new MemberAuthenticationSuccessHandler());
-//            authentication.setAuthenticationFailureHandler(new MemberAuthenticationFailureHandler());
-//
-//            JwtVerificationFilter verification = new JwtVerificationFilter(jwtTokenizer);
-//            builder .addFilter(authentication)
-//                    .addFilterAfter(verification, JwtAuthenticationFilter.class);
-//
-//        }
-//    }
-//
-//
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {return PasswordEncoderFactories.createDelegatingPasswordEncoder();}
+    private final JwtTokenizer jwtTokenizer;
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
+
+        http
+                .headers().frameOptions().sameOrigin()
+                .and()
+                .csrf().disable()
+                .cors(Customizer.withDefaults())
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .apply(new CustomFilterConfigurer())
+                .and()
+
+                .exceptionHandling()
+                .authenticationEntryPoint(new MemberAuthenticationEntryPoint())
+                .accessDeniedHandler(new MemberAccessDeniedHandler())
+
+                .and()
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+
+        return http.build();
+    }
+
+
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration cors = new CorsConfiguration();
+//        cors.setAllowCredentials(true);
+        cors.setAllowedOrigins(Arrays.asList("*")); //"http://localhost:3000"
+        cors.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE"));
+//        cors.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", cors);
+        return source;
+    }
+
+
+
+    public class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity builder) throws Exception {
+
+            AuthenticationManager manager = builder.getSharedObject(AuthenticationManager.class);
+            JwtAuthenticationFilter authentication = new JwtAuthenticationFilter(manager, jwtTokenizer);
+            authentication.setFilterProcessesUrl("/members/login");
+            authentication.setAuthenticationSuccessHandler(new MemberAuthenticationSuccessHandler());
+            authentication.setAuthenticationFailureHandler(new MemberAuthenticationFailureHandler());
+
+            JwtVerificationFilter verification = new JwtVerificationFilter(jwtTokenizer);
+            builder .addFilter(authentication)
+                    .addFilterAfter(verification, JwtAuthenticationFilter.class);
+
+        }
+    }
+
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {return PasswordEncoderFactories.createDelegatingPasswordEncoder();}
 
 }
