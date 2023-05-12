bbee5cc70295a9f1414607e29209122c5eeadf50
SOONUK-k <kswnuguri@gmail.com>
2023-05-12 21:44:00 +0900

    Chore : Oauth 설정파일 생성
    package 생성

---
+++ backend/src/main/java/com/codestates/auth/oauth/handler/OAuth2LoginSuccessHandler.java
@@ -0,0 +1,26 @@
+package com.codestates.auth.oauth.handler;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class OAuth2LoginSuccessHandler
+
+        implements AuthenticationSuccessHandler {
+
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+        AuthenticationSuccessHandler.super.onAuthenticationSuccess(request, response, chain, authentication);
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+    }
+}
