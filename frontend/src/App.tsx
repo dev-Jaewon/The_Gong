a0c9a39c4153e6d22953ca3fba42328b46f065ed
tari45800 <tari45800@gmail.com>
2023-05-15 17:41:43 +0900

    feat: carousel 화면크기 오류 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,8 @@
 import CreateRoom from "./kuronto/createRoom";
-import Carousel from "./components/Organisms/Carousel/Carousel";
-import TagForm from "./components/Organisms/TagForm";
 
 export const App = () => {
   return (
     <>
-      <TagForm></TagForm>
       <div>main</div>
     </>
   );
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { GlobalStyles } from './styles/GlobalStyle';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
+import Carousel from "./components/Organisms/Carousel/Carousel";
 import { Signup } from './pages/Signup/index.tsx';
+import styled from 'styled-components';
+
+const Stule = styled.div`
+
+`
 
 if (process.env.NODE_ENV === 'development') {
   const { worker } = require('./__mocks__/browser');
@@ -23,7 +28,10 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <GlobalStyles />
+      <Stule>
+       <GlobalStyles />
+      </Stule>
+      {/* <Carousel contentList={['1', '2', '3', '4', '5', '6', '7', '8']} contentNumber={2} contentwidth={50} contentheight={20} contentDot={true}></Carousel> */}
       <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />
     </QueryClientProvider>
   </React.StrictMode>
