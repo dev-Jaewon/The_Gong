fd76690446939595ee53964a2366269322457daf
gtaegyeong <gangtaegyeong12@gmail.com>
2023-05-18 15:36:42 +0900

    feat : room 구현 포스트맨 1차 테스트 완료

--- backend/src/main/java/com/codestates/room/dto/RoomTagDto.java
+++ backend/src/main/java/com/codestates/room/dto/RoomTagDto.java
@@ -2,12 +2,22 @@ package com.codestates.room.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class RoomTagDtos {
+public class RoomTagDto {
+    @Positive
+    private long tagId;
+    @NotBlank
     private String name;
 }
---
+++ backend/src/main/java/com/codestates/room/repository/RoomTagRepository.java
@@ -0,0 +1,7 @@
+package com.codestates.room.repository;
+
+import com.codestates.room.entity.RoomTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomTagRepository extends JpaRepository<RoomTag, Long> {
+}
