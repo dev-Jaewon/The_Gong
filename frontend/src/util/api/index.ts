49f984667943a18a6fef4215dd9536986c2a2e76
dev-Jaewon <app235naver@gmail.com>
2023-05-19 13:36:31 +0900

    [refact] 토큰 수정

--- frontend/src/util/api/index.ts
+++ frontend/src/util/api/index.ts
@@ -4,6 +4,9 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
+    Authorization: `Bearer ${JSON.parse(
+      localStorage.getItem('access_token') as string
+    )}`,
   },
   timeout: 2000,
 });
