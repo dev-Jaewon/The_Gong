a39899ea7b4d08ae40dec26e4aa48a5c97e14e2f
gtaegyeong <gangtaegyeong12@gmail.com>
2023-05-28 14:40:45 +0900

    feat : 닉네임 중복시 errorResponse 추가

--- backend/src/main/java/com/codestates/member/repository/MemberTagRepository.java
+++ backend/src/main/java/com/codestates/member/repository/MemberTagRepository.java
@@ -2,6 +2,7 @@ package com.codestates.member.repository;
 
 import com.codestates.member.entity.MemberTag;
 import com.codestates.tag.entity.Tag;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,7 @@ import java.util.Optional;
 
 @Repository
 public interface MemberTagRepository extends JpaRepository<MemberTag, Long> {
+
+    @EntityGraph(attributePaths = {"tag"}, type = EntityGraph.EntityGraphType.LOAD)
     Optional<MemberTag> findByTag(Tag tag);
 }
